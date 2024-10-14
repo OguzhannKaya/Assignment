f82323ca894f7fdc3dddb8fffce88ddb99afc265
OGUZHAN <OGUZHAN@OGUZHAN>
2024-10-14 19:18:15 +0300

    Add project files.

---
+++ BLL/DAL/Students.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BLL.DAL
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)] //Saati göz ardı etmek için öğrenip uyguladım.
+        public DateTime BirthDate { get; set; }
+        public decimal GPA { get; set; }
+    }
+}
